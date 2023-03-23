200f74c3cfdc571927c33ba68d5fac545844538f
Jéssika <jessikafujimura@gmail.com>
2023-03-22 23:56:53 -0300

    test: ParkingSpotController createParkingSpot

--- src/test/java/com/api/parkingcontrol/ParkingSpotControllerTest.java
+++ src/test/java/com/api/parkingcontrol/ParkingSpotControllerTest.java
@@ -1,7 +1,9 @@
 package com.api.parkingcontrol;
 
+import com.api.parkingcontrol.Exception.BusinessException;
 import com.api.parkingcontrol.Exception.NotFoundException;
 import com.api.parkingcontrol.controllers.ParkingSpotController;
+import com.api.parkingcontrol.dto.ParkingSpotDto;
 import com.api.parkingcontrol.models.ParkingSpotEntity;
 import com.api.parkingcontrol.services.ParkingSpotService;
 import org.junit.Assert;
@@ -18,6 +20,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -96,7 +99,96 @@ public class ParkingSpotControllerTest {
 
     @Test
     public void testCreateParkingSpot(){
+        ParkingSpotDto dto = new ParkingSpotDto();
+        dto.setParkingSpotNumber("number");
+        dto.setLicensePlateCar("license");
+        dto.setModelCar("model");
+        dto.setResponsibleName("responsible");
+        dto.setApartment("ape");
+        dto.setBlock("B");
+        dto.setBrandCar("brand");
+        dto.setColorCar("black");
 
+        ParkingSpotEntity ps1 = new ParkingSpotEntity();
+        ps1.setId(UUID.randomUUID());
+        ps1.setParkingSpotNumber("number");
+        ps1.setResponsibleName("responsible");
+        ps1.setColorCar("black");
+        ps1.setModelCar("model");
+        ps1.setApartment("ape");
+        ps1.setLicensePlateCar("license");
+        ps1.setRegistrationDate(LocalDateTime.now());
+        ps1.setBlock("B");
+        ps1.setBrandCar("brand");
+
+        when(parkingSpotService.save(any(ParkingSpotEntity.class))).thenReturn(ps1);
+
+        ResponseEntity<Object> response = parkingSpotController.createParkingSpot(dto);
+
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.CREATED);
+        Assert.assertNotNull(response.getBody());
     }
+
+    @Test
+    public void testCreateParkingSpotWithLicensePlateCarInUsed(){
+        ParkingSpotDto dto = new ParkingSpotDto();
+        dto.setParkingSpotNumber("number");
+        dto.setLicensePlateCar("license");
+        dto.setModelCar("model");
+        dto.setResponsibleName("responsible");
+        dto.setApartment("ape");
+        dto.setBlock("B");
+        dto.setBrandCar("brand");
+        dto.setColorCar("black");
+
+        Mockito.when(parkingSpotService.existsByLicensePlateCar("license")).thenReturn(true);
+
+        BusinessException exception = Assertions.assertThrows(BusinessException.class, () -> {
+            parkingSpotController.createParkingSpot(dto);
+        }, "License Plate Car is already in use!" );
+        Assert.assertEquals(exception.getErrorMessages(), Map.of("Conflict", List.of("License Plate Car is already in use!", HttpStatus.CONFLICT.toString())));
+    }
+
+    @Test
+    public void testCreateParkingSpotWithParkingSpotNumberInUsed(){
+        ParkingSpotDto dto = new ParkingSpotDto();
+        dto.setParkingSpotNumber("number");
+        dto.setLicensePlateCar("license");
+        dto.setModelCar("model");
+        dto.setResponsibleName("responsible");
+        dto.setApartment("ape");
+        dto.setBlock("B");
+        dto.setBrandCar("brand");
+        dto.setColorCar("black");
+
+        Mockito.when(parkingSpotService.existsByParkingSpotNumber("number")).thenReturn(true);
+
+        BusinessException exception = Assertions.assertThrows(BusinessException.class, () -> {
+            parkingSpotController.createParkingSpot(dto);
+        }, "Parking Spot is already in use!" );
+        Assert.assertEquals(exception.getErrorMessages(), Map.of("Conflict", List.of("Parking Spot is already in use!", HttpStatus.CONFLICT.toString())));
+    }
+
+    @Test
+    public void testCreateParkingSpotWithApartmentOrBlockInUsed(){
+        ParkingSpotDto dto = new ParkingSpotDto();
+        dto.setParkingSpotNumber("number");
+        dto.setLicensePlateCar("license");
+        dto.setModelCar("model");
+        dto.setResponsibleName("responsible");
+        dto.setApartment("ape");
+        dto.setBlock("B");
+        dto.setBrandCar("brand");
+        dto.setColorCar("black");
+
+        Mockito.when(parkingSpotService.existsByApartmentAndBlock("ape", "B")).thenReturn(true);
+
+        BusinessException exception = Assertions.assertThrows(BusinessException.class, () -> {
+            parkingSpotController.createParkingSpot(dto);
+        }, "Parking Spot already registered for this apartment/block!" );
+        Assert.assertEquals(exception.getErrorMessages(), Map.of("Conflict", List.of("Parking Spot already registered for this apartment/block!", HttpStatus.CONFLICT.toString())));
+    }
+
+
 
 }
